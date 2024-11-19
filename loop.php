eb4c63be074959522286bb4432488cab7d9add20
armando-9 <armandoza144@gmail.com>
2024-11-19 11:39:37 +0700

    2

--- loop.php
+++ loop.php
@@ -33,6 +33,30 @@
     for ($i=1; $i <= 10 ; $i++) { 
         echo"loop For รอบที่ :".$i."<br>";
     }
+    echo"<br>";
+    echo"----------------------------------------<br>";
+    echo"<h1>For loopซ้อนกัน</h1>";
+    for ($i=0; $i <=5 ; $i++) {
+        for ($j=0; $j <=5 ; $j++) { 
+            echo"*";
+        } 
+            echo"<br>";
+    }
+    echo"-------------------------------------------------------------------------------------<br>";
+    echo"<h1>Foreach loop แบบปกติ</h1>";
+    $pet = array("cat","dog","brid","fish","hippopotamus");
+    foreach ($pet as $val) {
+        echo"ฉันชอบ ".$val."<br>";
+    }
+    echo"----------------------------------------<br>";
+    echo"<h1>Foreach loop แบบkey =>value</h1>";
+    $food = array("A"=>"ผัดกระเพรา","B"=>"ผัดไท","C"=>"ผัดพริกแกง","D"=>"ไก่ย่าง","E"=>"ผัดซีอิ๊ว");
+    foreach ($food as $std => $val) {
+        echo "ชื่อ $std ชอบกิน$val<br>";
+    }
+
+
+        
 
 
 
